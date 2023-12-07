d1f1cd2ad40106040caa7e0bbb948b68b73aabcf
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2023-12-07 11:13:46 +0530

    Search Filter and Pagination

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -20,11 +20,15 @@ exports.createProduct = catchAsyncError( async (req, res, next)=>{
 
 exports.getAllProducts = catchAsyncError(async(req,res)=> {
 
-    const apiFeature = new ApiFeatures(Product.find, req.query).search
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeature.query;
     res.status(200).json({
         success:true,
-        products
+        products,
+        productCount,
     })
 })
 
--- server/utils/apifeatures.js
+++ server/utils/apifeatures.js
@@ -13,10 +13,36 @@ class ApiFeatures {
         }
         : {};
 
-        console.log(keyword);
-        this.query = this.queryStr.find({...keyword});
+        this.query = this.query.find({...keyword});
+        return this;
+    }
+
+    filter(){
+        const queryCopy = {...this.queryStr}
+        //Reamove some fields of category
+
+        const removeFields = ["keyword","page", "limit" ];
+
+        removeFields.forEach(key=> delete queryCopy[key]);
+       //filter for price
+
+       let queryStr = JSON.stringify(queryCopy);
+       queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g,key => `$${key}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+       
+        return this;
+    }
+
+    pagination(resultPerPage){
+        const currentPage = Number (this.queryStr.page)|| 1;
+
+        const skip = resultPerPage * (currentPage -1)
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+
         return this;
     }
 }
 
-module.exports = ApiFeatures
+module.exports = ApiFeatures;
