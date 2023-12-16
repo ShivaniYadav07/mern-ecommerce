bfcb2dc728295e46ea078b9ee638f20460b05618
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2023-12-16 14:03:12 +0530

    manage chakra-ui and mdb for footer

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,30 +1,132 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import { useColorMode, useColorModeValue  } from '@chakra-ui/react';
 
-const Footer = () => {
+export default function App() {
+  const { colorMode } = useColorMode();
+  
+  // Define a variable to store the background color based on the color mode
+  const footerBgColor = colorMode === 'current' ? 'current' : 'ghost';
+  const fontColor = useColorModeValue('black', '#bfb1d9');
+
   return (
-    <footer id='footer'>
-        <div className='leftFooter'>
-            <h4>Download Our App</h4>
-            <p>Download App for Android and IOS mobile phone</p>
-            <img src='' alt='playstore'/>
-            <img src='' alt='Appstore'/>
+    <MDBFooter bgColor={footerBgColor} color={fontColor}  className='text-center text-lg-start text-muted' >
+      <section className='d-flex justify-content-center justify-content-lg-between  border-bottom' >
+        <div className='me-5 d-none d-lg-block'>
+          <span style={{ color: fontColor }}>Get connected with us on social networks:</span>
+        </div>
 
+        <div style={{ color: fontColor }}>
+          <a href='/' className='me-4 text-reset' style={{ color: fontColor }}>
+            <MDBIcon  fab icon='facebook-f' />
+          </a>
+          <a href='/' className='me-4 text-reset' style={{ color: fontColor }}>
+            <MDBIcon  fab icon='twitter' />
+          </a>
+          <a href='/' className='me-4 text-reset' style={{ color: fontColor }}>
+            <MDBIcon  fab icon='google' />
+          </a>
+          <a href='/' className='me-4 text-reset'>
+            <MDBIcon  fab icon='instagram' />
+          </a>
+          <a href='/' className='me-4 text-reset'>
+            <MDBIcon  fab icon='linkedin' />
+          </a>
+          <a href='/' className='me-4 text-reset'>
+            <MDBIcon  fab icon='github' />
+          </a>
         </div>
-        <div className='middleFooter'>
-            <h1>Ecommerce</h1>
-            <p>High quality is our priority</p>
+      </section>
 
-            <p>Copyright 2023 &copy; priority</p>
-        </div>
-        <div className='rightFooter'>
-            <h4>Follow us</h4>
-            <a href='/'>Facebook</a>
-            <a href='/'>Facebook</a>
-            <a href='/'>Facebook</a>
-        </div>
-    </footer>
-  )
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4' style={{ color: fontColor }}>
+                <MDBIcon  icon='gem' className='me-3' />
+                Company name
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit.
+              </p>
+            </MDBCol>
+
+            <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4' style={{ color: fontColor }}>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Angular
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  React
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Vue
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Laravel
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4' style={{ color: fontColor }}>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4' style={{ color: fontColor }}>Contact</h6>
+              <p>
+                <MDBIcon  icon='home' className='me-2' style={{ color: fontColor }}/>
+                New York, NY 10012, US
+              </p>
+              <p>
+                <MDBIcon  icon='envelope' className='me-3' style={{ color: fontColor }}/>
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon  icon='phone' className='me-3' style={{ color: fontColor }}/> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon  icon='print' className='me-3' style={{ color: fontColor }}/> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)',  color: fontColor  }} >
+        © 2021 Copyright  
+        <a className='text-reset fw-bold' rel="noreferrer" target='_blank' href='https://shivaniyadav.online'>
+          || Shivani Yadav
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
-
-export default Footer
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import ColorModeSwitcher from './ColorModeSwitcher ';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
