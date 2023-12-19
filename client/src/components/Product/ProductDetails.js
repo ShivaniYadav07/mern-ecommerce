1c768c2f2e457db4bda57f156951810755e6a24a
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2023-12-19 12:59:50 +0530

    Added Product Details ad setting redux toolkit

--- client/src/components/Product/ProductDetails.js
+++ client/src/components/Product/ProductDetails.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react';
+import "./ProductDetails.css";
+import Carousel from "react-material-ui-carousel";
+import {useSelector, useDispatch} from "react-redux";
+import {getProductDetails} from "../../actions/productAction";
 
-const ProductDetails = () => {
+const ProductDetails = ({match}) => {
+  const dispatch = useDispatch();
+  const {product, loading, error} = useSelector((state)=>state.productDetails)
+  useEffect(() => {
+    dispatch(getProductDetails(match.params.id))
+  }, [dispatch, match.params.id])
   return (
-    <div>
-        
-    </div>
+    <Fragment>
+      <div className='productDetails'>
+        <div>
+        <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
+        </div>
+      </div>
+    </Fragment>
   )
 }
 
