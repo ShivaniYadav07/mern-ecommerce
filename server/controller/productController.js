bb2bd3ebae503ac96181f2a34688d9f3fac5fa01
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2023-12-20 21:26:52 +0530

    finish the redux implementation and all product details works well : )

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -157,64 +157,63 @@ exports.createProductReview = catchAsyncError(async (req, res, next) => {
   });
   
 
-  // Get All Reviews of a Product
+  // Get All Reviews of a product
+exports.getProductReviews = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
 
-  exports.getProductReviews = catchAsyncError(async (req, res, next) => {
-    const product = await Product.findById(req.query.id);
+  if (!product) {
+    return next(new ErrorHndler("Product not found", 404));
+  }
 
-    if(!product){
-        return next(new ErrorHndler("Product not found", 404));
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+// Delete Review
+exports.deleteReview = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+
+  if (!product) {
+    return next(new ErrorHndler("Product not found", 404));
+  }
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  let avg = 0;
+
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  let ratings = 0;
+
+  if (reviews.length === 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
     }
-    res.status(200).json({
-        success:true,
-        reviews: product.reviews,
-    });
-})
+  );
 
-    //Delete Review
-    exports.deleteReview = catchAsyncError(async (req, res, next) => {
-        const product = await Product.findById(req.query.productId);
-      
-        if (!product) {
-          return next(new ErrorHndler("Product not found", 404));
-        }
-      
-        const reviews = product.reviews.filter(
-          (rev) => rev._id.toString() !== req.query.id.toString()
-        );
-      
-        let avg = 0;
-      
-        reviews.forEach((rev) => {
-          avg += rev.rating;
-        });
-      
-        let ratings = 0;
-      
-        if (reviews.length === 0) {
-          ratings = 0;
-        } else {
-          ratings = avg / reviews.length;
-        }
-      
-        const numOfReviews = reviews.length;
-      
-        await Product.findByIdAndUpdate(
-          req.query.productId,
-          {
-            reviews,
-            ratings,
-            numOfReviews,
-          },
-          {
-            new: true,
-            runValidators: true,
-            useFindAndModify: false,
-          }
-        );
-      
-        res.status(200).json({
-          success: true,
-        });
-      });
-    
+  res.status(200).json({
+    success: true,
+  });
+});
