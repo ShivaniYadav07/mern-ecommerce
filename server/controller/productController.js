fe92296125870ba6425439f3cf01e35eb3da6982
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2023-12-31 19:48:21 +0530

    add to cart:)

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -23,17 +23,17 @@ exports.getAllProducts = catchAsyncError(async (req, res, next) => {
   const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
-    .filter()
-    .search();
+    .search()
+    .filter();
 
     
     let products = await apiFeature.query;
     
     apiFeature.pagination(resultPerPage);
-
-  let filteredProductsCount = products.length;
-
-  
+    
+    let filteredProductsCount = products.length;
+    
+    // products = await apiFeature.query;
 
 
   res.status(200).json({
