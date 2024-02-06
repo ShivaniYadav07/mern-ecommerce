f757c3e09ac1a30db2056d25196ece42e6a9ed00
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-02-06 15:53:09 +0530

    changes for deployment

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
   }
 app.use(express.json())
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb',  parameterLimit: 50000,}));
 app.use(fileUpload());
 
 //Route import
