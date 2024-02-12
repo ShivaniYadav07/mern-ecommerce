62e7362d5ccb3437eaac1bd20646629e5576c101
Ankur Agarwal <agarg2311@gmail.com>
2024-02-12 22:58:26 +0530

    domain added for cors

--- server/app.js
+++ server/app.js
@@ -4,26 +4,39 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middleware/error");
-const path = require('path');
-const cors = require('cors')
+const path = require("path");
+const cors = require("cors");
 //config
-if(process.env.NODE_ENV!=="PRODUCTION"){
-    require("dotenv").config({ path: 'server/config/config.env' });
-  }
-  app.use(cors({origin:"*", methods:"*", credentials: true, allowedHeaders:'*'}))
-  app.use(cookieParser());
-app.use(express.json())
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb',  parameterLimit: 50000,}));
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "server/config/config.env" });
+}
+app.use(
+  cors({
+    origin: ["https://mern-ecommerce-fc7df.web.app/"],
+    methods: "*",
+    credentials: true,
+    allowedHeaders: "*",
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+    parameterLimit: 50000,
+  })
+);
 app.use(fileUpload());
 
 //Route import
-const products = require('./routes/productRoute');
+const products = require("./routes/productRoute");
 const user = require("./routes/userRoute");
-const order = require("./routes/orderRoute")
-const payment = require("./routes/paymentRoute")
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
-app.use('/api/v1',products)
+app.use("/api/v1", products);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
@@ -31,11 +44,8 @@ app.use("/api/v1", payment);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Credentials', true);
-
-    res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
-})
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
 //Middleware for Error
 app.use(errorMiddleware);
 
