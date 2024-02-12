953510d50992147a3ed9287ae63c831603b51433
Ankur Agarwal <agarg2311@gmail.com>
2024-02-13 00:29:04 +0530

    localhost domain added in cors

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-  console.log("rennnnnnnnnnnnnnnnnnnnnnnn", req.cookies);
   const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("Please Login to access this resource", 401));
