ee0f6b8c09188c4da82596d6dbef0d00a74b9fc3
Ankur Agarwal <agarg2311@gmail.com>
2024-02-13 20:41:24 +0530

    cors options updated

--- server/app.js
+++ server/app.js
@@ -10,14 +10,14 @@ const cors = require("cors");
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "server/config/config.env" });
 }
-app.options("*", cors());
+// app.options("*", cors());
 app.use(
   cors({
-    origin: ["https://mern-ecommerce-fc7df.web.app", "http://localhost:3000"],
-    methods: "*",
+    origin: "*",
+    // methods: "*",
     credentials: true,
-    allowedHeaders: "*",
-    exposedHeaders: "*",
+    // allowedHeaders: "*",
+    // exposedHeaders: "*",
   })
 );
 app.use(cookieParser());
